1954ac23b3af8f4d6a7056d040e5fe90c5c97598
honghoker <ekflzhftk@naver.com>
2023-08-18 18:40:35 +0900

    lint test

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -13,11 +13,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
+        _ application:
+        
+        
+        
+        
+        UIApplication,
+        configurationForConnecting connectingSceneSession:
+        
+        
+        
+        
+        
+        
+        
+        UISceneSession,
+        options: UIScene
+            
+            
+            .ConnectionOptions
     ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name:
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(
