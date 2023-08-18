f5c8dc3db4d25edea084a37a81456521b3bd423f
honghoker <ekflzhftk@naver.com>
2023-08-18 18:11:03 +0900

    test pr

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -15,9 +15,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        options: UIScene
+            
+            
+            
+            .ConnectionOptions
+    )
+    
+    
+    
+    -> UISceneConfiguration {
+        return
+        UISceneConfiguration(
+            name: "Default Configuration", sessionRole: connectingSceneSession.role
+        
+        
+        
+        
+        
+        )
     }
 
     func application(
